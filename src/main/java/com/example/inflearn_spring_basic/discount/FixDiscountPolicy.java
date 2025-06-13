56c377c2606bcb0bd35d55e97dd4c5d961d19a79
Kimgyuill <rlarbdlf222@gmail.com>
2025-06-13 17:59:59 +0900

    15. 주문과 할인 도메인 개발

---
+++ src/main/java/com/example/inflearn_spring_basic/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package com.example.inflearn_spring_basic.discount;
+
+import com.example.inflearn_spring_basic.member.Grade;
+import com.example.inflearn_spring_basic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discounFixAmount = 1000; // 고정 할인 금액
+
+    @Override
+    public int discount(Member member, int discountAmount) {
+
+        if(member.getGrade() == Grade.VIP) {
+            return discounFixAmount; // VIP 회원에게는 고정 할인 금액 적용
+        }else {
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/com/example/inflearn_spring_basic/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.inflearn_spring_basic.order;
+
+import com.example.inflearn_spring_basic.discount.DiscountPolicy;
+import com.example.inflearn_spring_basic.discount.FixDiscountPolicy;
+import com.example.inflearn_spring_basic.member.Member;
+import com.example.inflearn_spring_basic.member.MemberRepository;
+import com.example.inflearn_spring_basic.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 회원 정보 조회
+        Member member = memberRepository.findById(memberId);
+        int discount = discountPolicy.discount(member, itemPrice);
+        // 주문 생성 로직
+        // 예시로 간단히 주문 객체를 생성하여 반환
+        return new Order(memberId, itemName, itemPrice, discount);
+    }
+
+
+}
