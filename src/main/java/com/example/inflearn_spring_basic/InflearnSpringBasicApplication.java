645d9134a1f8f407d3e2e34abc2708cba2a2dbd3
Kimgyuill <rlarbdlf222@gmail.com>
2025-06-13 16:51:10 +0900

    init: project initial setting

---
+++ src/main/java/com/example/inflearn_spring_basic/InflearnSpringBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_spring_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inflearn_spring_basic/InflearnSpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_spring_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
