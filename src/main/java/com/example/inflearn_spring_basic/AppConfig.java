4835caa61c6a8ae6d1b74521e007b13404f3afd5
Kimgyuill <rlarbdlf222@gmail.com>
2025-06-13 19:31:50 +0900

    20. AppConfig 리팩터링

--- src/main/java/com/example/inflearn_spring_basic/AppConfig.java
+++ src/main/java/com/example/inflearn_spring_basic/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.inflearn_spring_basic;
 
+import com.example.inflearn_spring_basic.discount.DiscountPolicy;
 import com.example.inflearn_spring_basic.discount.FixDiscountPolicy;
 import com.example.inflearn_spring_basic.member.MemberService;
 import com.example.inflearn_spring_basic.member.MemberServiceImpl;
@@ -8,11 +9,21 @@ import com.example.inflearn_spring_basic.order.OrderService;
 import com.example.inflearn_spring_basic.order.OrderServiceImpl;
 
 public class AppConfig {
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    private static MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 
+    public DiscountPolicy getDiscountPolicy(){
+        return new FixDiscountPolicy();
+        // return new RateDiscountPolicy(); // 다른 할인 정책을 사용하고 싶다면 이 줄을 주석 해제하고 위의 줄을 주석 처리하세요.
+    }
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
+    }
+
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), getDiscountPolicy());
     }
 }
