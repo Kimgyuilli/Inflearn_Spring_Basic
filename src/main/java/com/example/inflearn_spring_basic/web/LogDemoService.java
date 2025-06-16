0733bfd4646b84ea11fef29741ab6b7509ba270a
Kimgyuill <rlarbdlf222@gmail.com>
2025-06-16 20:50:24 +0900

    63. 스코프와 Provider

--- src/main/java/com/example/inflearn_spring_basic/web/LogDemoService.java
+++ src/main/java/com/example/inflearn_spring_basic/web/LogDemoService.java
@@ -2,15 +2,17 @@ package com.example.inflearn_spring_basic.web;
 
 import com.example.inflearn_spring_basic.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String testId) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("LogDemoService.logic(testId=" + testId + ") called");
     }
 }
